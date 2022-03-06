621a1249376f9343edcc405403eda9b69ac63ecf
JaStubblefield <cosmicpowerdeoxysattackforme@gmail.com>
2022-03-06 15:45:01 -0600

    Add project files.

---
+++ JStubblefield_Prog16/Default.aspx
@@ -0,0 +1,34 @@
+﻿<%--
+Jason Stubblefield
+Program 16, Due 5/8/18
+Partner names: None
+Description: This program prints the greeting and the text boxes for a basic calculator
+--%>
+
+
+<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="JStubblefield_Prog16.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <%
+        Response.Write("<h1>Welcome to the CIS 139 Calculator App<h1>");
+         %>
+        <p style="font-size: 12pt">
+        What is your name? <asp:TextBox ID="Name" runat="server"></asp:TextBox><br />
+        What operation would you like to do? Enter addition, subtraction, multiplication, or division: 
+        <asp:TextBox ID="Operation" runat="server"></asp:TextBox><br />
+        Enter the first number: <asp:TextBox ID="X" runat="server"></asp:TextBox><br />
+        Enter the second number: <asp:TextBox ID="Y" runat="server"></asp:TextBox><br />
+        <asp:Button ID="Calculate" runat="server" Text="Calculate!" />
+        </p>
+    </div>
+    </form>
+</body>
+</html>
---
+++ JStubblefield_Prog16/Default.aspx.cs
@@ -0,0 +1,71 @@
+﻿/*Jason Stubblefield
+ * Program 16, Due 5/8/18
+ * Partner names: None
+ * Description: This program validates the user's input and does calculations on it
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace JStubblefield_Prog16
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack)
+            {
+                form1.Visible = false;
+                double num1;
+                double num2;
+                double result = 0;
+                char symbol = 'N';
+                if (double.TryParse(X.Text, out num1) && double.TryParse(Y.Text, out num2))
+                {
+                    switch (Operation.Text.ToUpper())
+                    {
+                        case "ADDITION":
+                            symbol = '+';
+                            result = num1 + num2;
+                            break;
+                        case "SUBTRACTION":
+                            symbol = '-';
+                            result = num1 - num2;
+                            break;
+                        case "MULTIPLICATION":
+                            symbol = '*';
+                            result = num1 * num2;
+                            break;
+                        case "DIVISION":
+                            if (Math.Abs(num2) < 0.0000001)
+                            {
+                                Response.Write("Cannot divide by zero. Hit the back button and try again.");
+                            }
+                            else
+                            {
+                                symbol = '/';
+                                result = num1 / num2;
+                            }
+                            break;
+                        default:
+                            Response.Output.Write("Sorry, {0}, I am unable to determine the operation you desire. Hit the back button and try again.", Name.Text);
+                            break;
+                    }
+                    if (symbol != 'N')
+                    {
+                        Response.Output.Write("{0}, here is your result: {1} {2} {3} = {4}<br /><br />", Name.Text, num1, symbol, num2, result);
+                        Response.Write("If you have another calculation, please hit the Back button and enter your new information.");
+                    }
+                }
+                else
+                {
+                    Response.Output.Write("Sorry, {0}, your numbers are invalid. Hit the Back button and try again.", Name.Text);
+                }
+            }
+        }
+    }
+}
